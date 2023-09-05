daed2c3cdec405220821d3144f5734a1c1c4b22a
amane-uehara <62415046+amane-uehara@users.noreply.github.com>
2023-09-05 19:33:18 +0900

    add common script to show time and mac addr

---
+++ common/fetch_run.py
@@ -0,0 +1,18 @@
+import sys
+import json
+from datetime import datetime
+from uuid import getnode as get_mac
+
+def fetch_run():
+  ret = {}
+  ret['dt']  = datetime.now().strftime("%Y%m%d%H%M%S")
+  ret['mac_addr'] = hex(get_mac())[2:]
+
+  return ret
+
+if __name__ == "__main__":
+  print(sys.argv)
+  data = fetch_raw_system_resource.fetch_data()
+  run  = fetch_run()
+  run["data"] = data
+  print(json.dumps(run))
