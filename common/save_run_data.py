149381fbf1b3015c325a3cf94da153a83d2f0924
amane-uehara <62415046+amane-uehara@users.noreply.github.com>
2023-09-07 02:04:38 +0900

    separate save_file function from save_raw_data.py to (new) save_run_data.py

---
+++ common/save_run_data.py
@@ -0,0 +1,18 @@
+import os
+import json
+from read_config import apply_template
+from fetch_run import fetch_run
+
+def save_run_data(data, title, raw_config):
+  run  = fetch_run()
+  run["hostname"] = raw_config["constant"]["hostname"]
+  run["data"] = data
+
+  config = apply_template(raw_config, run["dt"])
+
+  filename = config[title]["save_raw_file"]
+  filepath = os.path.dirname(filename)
+  os.makedirs(filepath, mode=0o777, exist_ok=True)
+
+  with open(filename, 'w') as f:
+    f.write(json.dumps(run))
--- system_resource/save_raw_data.py
+++ system_resource/save_raw_data.py
@@ -3,9 +3,9 @@ from pathlib import Path
 SCRIPT_ROOT = Path(__file__).resolve().parent.parent
 
 sys.path.append(str(SCRIPT_ROOT.joinpath('common')))
-from fetch_run import fetch_run
 import read_config
 from fetch_raw_data import fetch_raw_data
+from save_run_data  import save_run_data
 
 import json
 import os
@@ -13,21 +13,10 @@ import os
 def main(argv):
   title = argv[1]
   raw_config = read_config.read_raw_config(argv)
-
   data = fetch_raw_data()
-  run  = fetch_run()
-  run["hostname"] = raw_config["constant"]["hostname"]
-  run["data"] = data
 
-  config = read_config.apply_template(raw_config, run["dt"])
-
-  filename  = config[title]["save_raw_file"]
-  filepath  = os.path.dirname(filename)
-  os.makedirs(filepath, mode=0o777, exist_ok=True)
-
-  with open(filename, 'w') as f:
-    f.write(json.dumps(run))
+  save_run_data(data, title, raw_config)
 
 if __name__ == "__main__":
   main(sys.argv)
-  # python3 save_raw_data.py "system_resource" [--config=/path/to/config.json]
+  # python3 save_raw_data.py "json_job_title" [--config=/path/to/config.json]
