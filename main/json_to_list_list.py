eb55ddb1d2cdf64e46bc4e5387165f66c63c6b0a
amane-uehara <62415046+amane-uehara@users.noreply.github.com>
2023-09-08 22:52:24 +0900

    recursive search for csv key value

--- main/json_to_list_list.py
+++ main/json_to_list_list.py
@@ -1,17 +1,15 @@
 import json
+import sys
 from json_to_json_list import fetch_json_list
 from common.save_run_data import default_run_key_list
 
 def fetch_list_list(config, default_data_key_list):
-  if "run_key_list" in config:
-    run_key_list = config["run_key_list"]
-  else:
-    run_key_list = default_run_key_list()
-
-  if "data_key_list" in config:
-    data_key_list = config["data_key_list"]
-  else:
-    data_key_list = default_data_key_list()
+  if "run_key_list"  in config: run_key_list  = config["run_key_list"]
+  else:                         run_key_list  = default_run_key_list()
+  if "data_key_list" in config: data_key_list = config["data_key_list"]
+  else:                         data_key_list = default_data_key_list()
+  key_list = run_key_list + list(map(lambda x: "data." + x, data_key_list))
+  print("csv_key: " + str(key_list), file=sys.stderr)
 
   json_list = fetch_json_list(config)
 
@@ -19,19 +17,18 @@ def fetch_list_list(config, default_data_key_list):
 
   for run in json_list:
     tmp = []
-
-    for key in run_key_list:
-      if key not in run:
-        tmp.append("")
-      else:
-        tmp.append(run[key])
-
-    for key in data_key_list:
-      if key not in run["data"]:
-        tmp.append("")
-      else:
-        tmp.append(run["data"][key])
-
+    for dot_key in key_list:
+      tmp.append(key_search(dot_key.split("."), run))
     ret.append(tmp)
 
   return ret
+
+def key_search(layer_list, data_dict):
+  key = layer_list.pop(0)
+
+  if key not in data_dict:
+    return ""
+  elif len(layer_list) == 0:
+    return data_dict[key]
+  elif len(layer_list) >= 1:
+    return key_search(layer_list, data_dict[key])
