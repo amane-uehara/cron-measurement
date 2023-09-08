dd6c25ece0e883da3adb5d45925a7bef35f94727
amane-uehara <62415046+amane-uehara@users.noreply.github.com>
2023-09-08 17:43:07 +0900

    refactor common scripts

--- main/common/save_run_data.py
+++ main/common/save_run_data.py
@@ -1,16 +1,20 @@
 import os
 import json
-from common.read_config import apply_template
-from common.fetch_run import fetch_run
+from datetime import datetime
+from uuid import getnode as get_mac
+from common.read_config import apply_time_template
 
-def save_run_data(data, title, raw_config):
-  run  = fetch_run()
-  run["hostname"] = raw_config["constant"]["hostname"]
+def save_run_data(data, config):
+  run = {}
+  run['dt']  = datetime.now().strftime("%Y%m%d%H%M%S")
+  run["hostname"] = config["hostname"]
+  run['mac_addr'] = hex(get_mac())[2:]
+  if "sensor_mac_addr" in config:
+    run['sensor_mac_addr'] = config["sensor_mac_addr"].lower().replace(':','')
   run["data"] = data
 
-  config = apply_template(raw_config, run["dt"])
-
-  filename = config[title]["save_raw_file"]
+  apply_time = apply_time_template(config, run["dt"])
+  filename = apply_time["save_file"]
   filepath = os.path.dirname(filename)
   os.makedirs(filepath, mode=0o777, exist_ok=True)
 
--- main/main.py
+++ main/main.py
@@ -1,45 +1,42 @@
 import sys
 import json
 import gzip
-import common.json_to_list_gz
-from common.read_config import read_raw_config, apply_template
+from common.read_config import read_config, apply_time_template
 from common.save_run_data import save_run_data
 from common.fetch_filelist import fetch_filelist
 
 def main(argv):
   title = argv[1]
-  raw_config = read_raw_config(argv)
-  program = raw_config[title]["program"]
+  config = read_config(argv)
+  program = config["program"]
 
   if program == "fetch_system_resource":
     import system_resource
     data = system_resource.fetch_json()
-    save_run_data(data, title, raw_config)
+    save_run_data(data, config)
 
   if program == "fetch_omron_env_sensor":
     import omron_env_sensor
-    mac_addr = raw_config[title]["sensor_mac_addr"].lower().replace(':','')
-    bt_retry_count = raw_config[title]["bt_retry_count"]
-    bt_dev_id      = raw_config[title]["bt_dev_id"]
+    mac_addr       = config["sensor_mac_addr"].lower().replace(':','')
+    bt_retry_count = config["bt_retry_count"]
+    bt_dev_id      = config["bt_dev_id"]
     data = omron_env_sensor.fetch_json(mac_addr, bt_retry_count, bt_dev_id)
-    data["sensor_mac_addr"] = mac_addr
-    save_run_data(data, title, raw_config)
+    save_run_data(data, config)
 
   if program == "fetch_inkbird_env_sensor":
     import inkbird_env_sensor
-    mac_addr = raw_config[title]["sensor_mac_addr"].lower().replace(':','')
-    bt_retry_count = raw_config[title]["bt_retry_count"]
+    mac_addr       = config["sensor_mac_addr"].lower().replace(':','')
+    bt_retry_count = config["bt_retry_count"]
     data = inkbird_env_sensor.fetch_json(mac_addr, bt_retry_count)
-    data["sensor_mac_addr"] = mac_addr
-    save_run_data(data, title, raw_config)
+    save_run_data(data, config)
 
   if program == "json_to_list":
     yyyymmddhhmmss = argv[2]
-    config = apply_template(raw_config, yyyymmddhhmmss)
-    print(config[title]["load_json_files"])
-    load_json_files = fetch_filelist(config[title]["load_json_files"])
+    config = apply_time_template(config, yyyymmddhhmmss)
+    print(config["load_json_files"])
+    load_json_files = fetch_filelist(config["load_json_files"])
     print(load_json_files)
-    save_file = config[title]["save_json_gz_file"]
+    save_file = config["save_file"]
     join_list = []
     for json_file in load_json_files:
       with open(json_file, 'r') as f:
