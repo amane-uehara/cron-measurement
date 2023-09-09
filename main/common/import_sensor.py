74b0750400b98f4db00526a7d45bffc86c0f4912
amane-uehara <62415046+amane-uehara@users.noreply.github.com>
2023-09-09 16:02:23 +0900

    add import_sensor

--- main/common/import_sensor.py
+++ main/common/import_sensor.py
@@ -1,23 +1,24 @@
-from glob import glob
 import os
+import sys
+from importlib import import_module
+from glob import glob
 
-def fetch_sensor_list():
+def fetch_sensor_name_list():
   main_path   = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
   sensor_path = os.path.join(main_path, "sensor", "*.py")
   sensor_file_list = glob(sensor_path)
   sensor_name_list = list(map(lambda x: str(os.path.basename(x))[:-3], sensor_file_list))
-  print(sensor_name_list)
-  #load_file_list = fetch_filelist(config["load_file_list"])
-  #print('abs dirname: ', os.path.dirname(os.path.abspath(__file__)))
+  return sensor_name_list
 
-  #join_list = []
-  #for json_file in load_file_list:
-  #  with open(json_file, "r") as f:
-  #    json_data = json.load(f)
-  #    if isinstance(json_data, dict):
-  #      join_list.append(json_data)
-  #    if isinstance(json_data, list):
-  #      join_list += json_data
-
-  #sorted_list = sorted(join_list, key=custom_sort)
-  #return sorted_list
+def import_sensor(sensor_name):
+  ret = {}
+  sensor_name_list = fetch_sensor_name_list()
+  if not sensor_name in sensor_name_list:
+    print("ERROR: sensor '" + sensor_name + "' is not in /main/sensor/* " + str(sensor_name_list), file=sys.stderr)
+    sys.exit(1)
+  else:
+    print("INFO: import sensor." + sensor_name, file=sys.stderr)
+    module = import_module("sensor." + sensor_name)
+    ret["fetch_json"] = module.fetch_json
+    ret["key_list"]   = module.key_list
+  return ret
--- main/main.py
+++ main/main.py
@@ -1,33 +1,27 @@
 import sys
 import json
-from importlib import import_module
 from common.read_config_file import read_config_file, apply_time_template
 from common.save_file import save_file, save_run_data
 from common.read_json_files import fetch_list_list, fetch_json_list
-import common.import_sensor
+from common.import_sensor import import_sensor
 
 def main(argv):
   config  = read_config_file(argv)
-  sensor  = config["sensor"]
+
+  sensor = import_sensor(config["sensor"])
+  fetch_json = sensor["fetch_json"]
+  key_list   = sensor["key_list"]
+
   program = config["program"]
 
-  if not sensor:
-    sensor_fetch_json = None
-    sensor_key_list = None
-  else:
-    common.import_sensor.fetch_sensor_list()
-    module = import_module("sensor." + sensor)
-    sensor_fetch_json = module.fetch_json
-    sensor_key_list = module.key_list
-
   if program == "fetch_raw_json":
-    data = sensor_fetch_json(config)
+    data = fetch_json(config)
     save_run_data(data, config)
 
   if program == "to_csv":
     yyyymmddhhmmss = argv[2]
     apply_time = apply_time_template(config, yyyymmddhhmmss)
-    value_list_list = fetch_list_list(apply_time, sensor_key_list())
+    value_list_list = fetch_list_list(apply_time, key_list())
     csv_text = "\n".join([",".join(map(str, x)) for x in value_list_list])
     save_file(csv_text, apply_time)
 
