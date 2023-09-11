5c81659f493b3fbde2a43cc26e8fb282156f7226
amane-uehara <62415046+amane-uehara@users.noreply.github.com>
2023-09-11 22:11:53 +0900

    add search_write function

--- main/common/trans_json.py
+++ main/common/trans_json.py
@@ -1,7 +1,7 @@
 import sys
 from math import floor
 
-def key_search(layer_list, data_dict):
+def search_read(layer_list, data_dict):
   key = layer_list.pop(0)
 
   if key not in data_dict:
@@ -9,7 +9,20 @@ def key_search(layer_list, data_dict):
   elif len(layer_list) == 0:
     return data_dict[key]
   elif len(layer_list) >= 1:
-    return key_search(layer_list, data_dict[key])
+    return search_read(layer_list, data_dict[key])
+
+def search_write(layer_list, value, data_dict):
+  ret = data_dict.copy()
+
+  tmp = ret
+  while len(layer_list) >= 2:
+    key = layer_list.pop(0)
+    if key not in tmp:
+      tmp[key] = {}
+    tmp = tmp[key]
+
+  tmp[layer_list[0]] = value
+  return ret
 
 def default_run_key_list():
   return [
@@ -32,7 +45,7 @@ def trans_to_list_list(json_list, config, default_data_key_list):
   for run in json_list:
     tmp = []
     for dot_key in key_list:
-      tmp.append(key_search(dot_key.split("."), run))
+      tmp.append(search_read(dot_key.split("."), run))
     ret.append(tmp)
 
   return ret
@@ -49,7 +62,7 @@ def trans_to_selected_json_list(json_list, config):
   for run in json_list:
     tmp = {}
     for input_key, output_key in key_dict.items():
-      tmp[output_key] = key_search(input_key.split("."), run)
+      tmp[output_key] = search_read(input_key.split("."), run)
     ret.append(tmp)
 
   return ret
@@ -67,35 +80,48 @@ def trans_to_percentile_json_list(json_list, config, default_data_key_list):
   else:                         run_key_list  = default_run_key_list()
   if "data_key_list" in config: data_key_list = config["data_key_list"]
   else:                         data_key_list = default_data_key_list
+  key_list = run_key_list + list(map(lambda x: "data." + x, data_key_list))
+  print("INFO: csv_key: " + str(key_list), file=sys.stderr)
 
-  if "percentile" in run_key_list:
-    del run_key_list["percentile"]
+  if "percentile" in key_list:
+    del key_list["percentile"]
 
   ret = []
   for i in range(div):
     ret.append({"percentile":i})
 
-  for run_key in run_key_list:
+  for key in key_list:
     tmp = []
     for run in json_list:
-      tmp.append(key_search(run_key.split("."), run))
+      tmp.append(search_read(key.split("."), run))
     tmp.sort()
 
     for i in range(div):
       index = int((len(tmp)-1)*i/div)
-      ret[i][run_key] = tmp[index]
+      value = tmp[index]
+      ret[i] = search_write(key.split("."), value, ret[i])
 
-  for i in range(div):
-    ret[i]["data"] = {}
+  # for run_key in run_key_list:
+  #   tmp = []
+  #   for run in json_list:
+  #     tmp.append(search_read(run_key.split("."), run))
+  #   tmp.sort()
 
-  for data_key in data_key_list:
-    tmp = []
-    for run in json_list:
-      tmp.append(key_search(("data."+data_key).split("."), run))
-    tmp.sort()
+  #   for i in range(div):
+  #     index = int((len(tmp)-1)*i/div)
+  #     ret[i][run_key] = tmp[index]
 
-    for i in range(div):
-      index = int((len(tmp)-1)*i/div)
-      ret[i]["data"][data_key] = tmp[index]
+  # for i in range(div):
+  #   ret[i]["data"] = {}
+
+  # for data_key in data_key_list:
+  #   tmp = []
+  #   for run in json_list:
+  #     tmp.append(search_read(("data."+data_key).split("."), run))
+  #   tmp.sort()
+
+  #   for i in range(div):
+  #     index = int((len(tmp)-1)*i/div)
+  #     ret[i]["data"][data_key] = tmp[index]
 
   return ret
