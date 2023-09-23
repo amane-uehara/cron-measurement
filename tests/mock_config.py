3ff8a303fd58f00b8945530e4076ccab9cbd5d87
Amane Uehara <62415046+amane-uehara@users.noreply.github.com>
2023-09-23 20:53:30 +0900

    add testcase in join_arg_config

--- tests/mock_config.py
+++ tests/mock_config.py
@@ -85,3 +85,54 @@ def mock_config_replace_test():
     "mac_addr": "1122334455ff",
     "hostname": "example_host"
   }
+
+# ----------------------------------------------------------------------------------------------------
+
+def mock_arg_exec_test():
+  return {
+    "title": "config_title_1",
+    "yyyymmddhhmmss": "20220304050607",
+    "config": "./config.json",
+    "dryrun": "True",
+    "current_path": "/path/to/current_path",
+    "repository_path": "/path/to/repo_path",
+    "default_config_file": "/path/to/repo_path/config.json",
+    "sys_executable": "/usr/bin/python3",
+    "now_yyyymmddhhmmss": "20300101010101",
+    "mac_addr": "1122334455ff",
+    "exec_main_py": "/usr/bin/python3 main/main.py",
+    "config_abspath": "/path/to/repo_path/config.json"
+  }
+
+def mock_raw_config_exec_test():
+  return {
+    "constant": {
+      "hostname": "example_host",
+    },
+    "config_title_0": {
+      "will_be": "igonred"
+    },
+    "config_title_1": {
+      "exec": "${fork_main_py} foobar --yyyymmddhhmmss: ${yyyymmddhh-1}0000" 
+    },
+    "config_title_2": {
+      "will_be": "igonred"
+    }
+  }
+
+def mock_config_exec_test():
+  return {
+    "exec": "/usr/bin/python3 main/main.py --config /path/to/repo_path/config.json --dryrun foobar --yyyymmddhhmmss: 20220304040000",
+    "hostname": "example_host",
+    "yyyymmddhhmmss": "20220304050607",
+    "yyyymmddhhmm": "202203040506",
+    "yyyymmddhh": "2022030405",
+    "yyyymmdd": "20220304",
+    "yyyymm": "202203",
+    "yyyy": "2022",
+    "repository_path": "/path/to/repo_path",
+    "dryrun": "True",
+    "fork_main_py": "/usr/bin/python3 main/main.py --config /path/to/repo_path/config.json --dryrun",
+    "mac_addr": "1122334455ff",
+    "hostname": "example_host"
+  }
